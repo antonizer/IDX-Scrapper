32b7bd5d4d7525e9a8091acb7d34d78abe693b90
Wildan Gunawan <gwildan50@gmail.com>
2021-01-23 12:34:34 +0800

    Remove kaggle stuff

--- first-time.sh
+++ first-time.sh
@@ -6,13 +6,6 @@ mkdir -p "data/Saham/LQ45"
 # install requirements
 pip3 install -r requirements.txt
 
-# kaggle env
-export KAGGLE_USERNAME=
-export KAGGLE_KEY=
-
-# get metadata
-kaggle datasets metadata -p data tiwill/saham-lq45-idx
-
 # start script
 python3 get-list-emiten.py
 python3 get-data-tahunan-emiten.py
